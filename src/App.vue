2f67b2e3bcde4fa9fdece5b25ece980b3c831b06
nettamei <nettamei@post.bgu.ac.il>
2024-06-09 18:08:41 +0300

    starting navbar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>|
-      <router-link :to="{ name: 'search' }">Search</router-link>|
-      {{ !$root.store.username }}
-      <span v-if="!$root.store.username">
-        Guest:
-        <router-link :to="{ name: 'register' }">Register</router-link>|
-        <router-link :to="{ name: 'login' }">Login</router-link>|
-      </span>
-      <span v-else>
-        {{ $root.store.username }}: <button @click="Logout">Logout</button>|
-      </span>
-    </div>
+    <nav class="navbar navbar-expand-lg bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <router-link class="navbar-brand" :to="{ name: 'main' }">Vue Recipes</router-link>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 align-items-center">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{ name: 'search' }">Search</router-link>
+          </li>
+          <li class="nav-item" v-if="!$root.store.username">
+            Hello Guest: <router-link class="nav-link d-inline" :to="{ name: 'register' }">Register</router-link>
+          </li>
+          <li class="nav-item" v-if="!$root.store.username">
+            <router-link class="nav-link" :to="{ name: 'login' }">Login</router-link>
+          </li>
+          <li class="nav-item" v-if="!$root.store.username">
+            <router-link class="nav-link" :to="{ name: 'about' }">About</router-link>
+          </li>
+          <li class="nav-item" v-else>
+            {{ $root.store.username }}: <button class="btn btn-link nav-link" @click="Logout">Logout</button>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
@@ -44,16 +60,24 @@ export default {
   min-height: 100vh;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
+.nav-item {
   font-weight: bold;
-  color: #2c3e50;
+  color: #8e9498;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav-link {
+  color: #898d90;
+}
+
+.nav-link.router-link-exact-active {
+  color: #898d90;
+}
+
+.d-inline {
+  display: inline;
+}
+
+.align-items-center {
+  align-items: center !important;
 }
 </style>
--- src/routes.js
+++ src/routes.js
@@ -23,7 +23,7 @@ const routes = [
     component: () => import("./pages/SearchPage"),
   },
   {
-    path: "   ",
+    path: "/recipes",
     name: "recipe",
     component: () => import("./pages/RecipeViewPage"),
   },
